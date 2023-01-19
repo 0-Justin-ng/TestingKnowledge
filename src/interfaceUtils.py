fa8e66482a3e276d5c2738a236f13fcc98ff8955
0-justin-ng <0.justin.ng@gmail.com>
2023-01-19 17:31:49 -0500

    Initial commit.

---
+++ src/interfaceUtils.py
@@ -0,0 +1,22 @@
+import questions
+import tkinter as tk
+
+
+def parse_questions(discipline, topic, question, answer):
+    discipline_response = discipline.get()
+    topic_response = topic.get()
+    question_response = question.get()
+    answer_response = answer.get()
+
+    print(discipline_response, topic_response, question_response, answer_response)
+    return discipline_response, topic_response, question_response, answer_response
+
+def store_question(discipline, topic, question, answer):
+    responses = parse_questions(discipline, topic, question, answer)
+    questions.add_question(responses)
+    return None
+
+def clear_button(responses):
+    for response in responses:
+        response.delete(0, tk.END)
+    return None
