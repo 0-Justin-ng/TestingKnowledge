495eed44d31a7236b379294411f68b4fb4da1055
0-justin-ng <0.justin.ng@gmail.com>
2023-01-20 16:40:58 -0500

    Fixed handling of index when there are no questions in a csv file.

--- src/questions.py
+++ src/questions.py
@@ -14,19 +14,20 @@ INITIAL_ROW = ['index','topic', 'question', 'answer']
 # /home/usr/TestingKnowledge/src , /home/usr/TestingKnowledge, /home/jng, /home, /
 # Index at 1 to select a path one level up from the current directory where this file is located.
 p = Path(__file__).parents[1]
-path_to_questions = p / 'questions'
+PATH_TO_QUESTIONS = p / 'questions'
+
 
 def _initialize_questions():
     # Sets up question csvs to start storing questions. 
-    if len(os.listdir(path_to_questions)) < 4:
+    if len(os.listdir(PATH_TO_QUESTIONS)) < 4:
         for discipline in MAIN_DISCIPLINES:
-            discipline_path = path_to_questions / f'{discipline}.csv'
+            discipline_path = PATH_TO_QUESTIONS / f'{discipline}.csv'
             with open(discipline_path, 'a') as file_object:
                 writer_object = writer(file_object)
                 writer_object.writerow(INITIAL_ROW)
 
 def _get_csv_filepath(discipline):
-    file_path = path_to_questions / discipline 
+    file_path = PATH_TO_QUESTIONS / discipline 
     return f'{file_path}.csv'
 
 
@@ -35,18 +36,26 @@ def add_question(responses):
     discipline = responses[0]
     topic = responses[1]
     question = responses[2]
-    answer = responses[3]
+    # Need to strip extra whitespace from text field from tkinter.
+    answer = responses[3].strip()
 
     file_path = _get_csv_filepath(discipline)
 
     df = pd.read_csv(file_path)
-    new_index=list(df.index.values)[-1] + 1
+    
+    # This is sets the correct initial index if there are no questions.
+    if len(df.index.values) == 0:
+        new_index = 0
+    else:
+        new_index=list(df.index.values)[-1] + 1
+
     df.loc[new_index] = [0, topic, question, answer]
     df['index'] = list(df.index.values)
 
     df.to_csv(file_path, index=False)
    
     return 'Done adding question.'
+
 
 def remove_question(discipline, indices):
 
@@ -60,11 +69,10 @@ def remove_question(discipline, indices):
 
     df.to_csv(file_path, index=False)
 
+
 def get_random_question(discipline):
     file_path = _get_csv_filepath(discipline)
     df = pd.read_csv(file_path)
-
-    print(len(df))
 
     random_index = randint(0, len(df)-1)
 
